9064df3ee86fde2b5efc922757531b3f068b547f
Alishia Subramoney <subramoneyalishia@gmail.com>
2023-01-16 21:11:02 +0200

    Commit file

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python3
-""" Extends your Python script to export data in the CSV format """
+"""Exports to-do list information for a given employee ID to CSV format."""
 import csv
 import requests
 import sys
