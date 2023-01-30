f615e374fc4bd5e8424a5e3a0857b5afe7a73e98
Alishia Subramoney <subramoneyalishia@gmail.com>
2023-01-30 21:38:13 +0200

    Readme

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# Using Puppet to create a file in /tmp.
+
+file { 'school' :
+ensure  => present ,
+path    => '/tmp/school'.
+content => 'I love puppet',
+owner   => 'www-data',
+group   => 'www-data',
+mode    => 0744,
+}
