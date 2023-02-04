678fef88fe44b7519b89a38aff69daa4a3e94900
Alishia Subramoney <subramoneyalishia@gmail.com>
2023-02-04 17:34:30 +0200

    Commit file

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,5 @@
+# SSH client configuration in order to use the private key ~/.ssh/school
+# SSH client configuration in order to refuse to authenticate using a password
+
+exec { "/bin/sed -i '0,/.*PasswordAuthentication.*/{s/.*PasswordAuthentication.*/    PasswordAuthentication no/}' /etc/ssh/ssh_config": }
+exec { "/bin/sed -i '0,/.*IdentityFile.*/{s/.*IdentityFile.*/    IdentityFile \~\/\.ssh\/school/}' /etc/ssh/ssh_config": }
